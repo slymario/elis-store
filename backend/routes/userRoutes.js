502b57fbec428dc23c19eea9f202990d27b34d36
slymario <nwobodo_obinna@yahoo.com>
2024-03-13 17:50:10 +0100

    Admin route, get/update/delete user by ID functionality added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,25 @@
 import express from "express";
-import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile } from "../controllers/userController.js";
+import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile, deleteUserById, getUserById, updateUserById } from "../controllers/userController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router.route("/")
+.post(createUser)
+.get(authenticate, authorizeAdmin, getAllUsers);
+
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
-router.route("/profile").get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
+
+router.route("/profile")
+.get(authenticate, getCurrentUserProfile)
+.put(authenticate, updateCurrentUserProfile);
+
+//ADMIN ROUTE
+router.route("/:id")
+.delete(authenticate, authorizeAdmin, deleteUserById)
+.get(authenticate, authorizeAdmin, getUserById)
+.put(authenticate, authorizeAdmin, updateUserById);
 
 
 export default router;
