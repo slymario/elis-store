3b1d618cbac6ae938e8c7c403d701a75914e7ca3
slymario <nwobodo_obinna@yahoo.com>
2024-03-15 19:43:27 +0100

    Admin Route created

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store.js'
 import Login from './pages/Auth/Login.jsx';
 import Register from './pages/Auth/Register.jsx';
+import AdminRoute from './pages/Admin/AdminRoute.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import Profile from './pages/User/Profile.jsx';
 
@@ -15,11 +16,17 @@ import Profile from './pages/User/Profile.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+
       <Route path='' element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />} />
       </Route>
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
+      
+
+      <Route path='/admin' element={<AdminRoute />}>
+        
+      </Route>
     </Route>
   )
 );
